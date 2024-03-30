3060ba77decaeade996aa416c23b3c58b0321463
Pyanuk <144242312+Pyanuk@users.noreply.github.com>
2024-03-30 11:20:13 +0300

    akva

--- Akvarium.cpp
+++ Akvarium.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
@@ -17,7 +18,6 @@ public:
         FriendlyFish(string frendlyfishs, int size, string view, double weight, string foot)
             :Frendlyfishs(frendlyfishs), Size(size), View(view), Weight(weight), Foot(foot) {}
 
-
         void CreateFriendlyFish() {
             setlocale(LC_ALL, "Rus");
             cout << "Введите не хищную рыбы: " << Frendlyfishs << endl;
@@ -26,20 +26,19 @@ public:
             cin >> Size;
             cout << "Вид рыбы: " << View << endl;
             cin >> View;
-            cout << "Вес рыбкы: " << Weight << endl;
+            cout << "Вес рыбы: " << Weight << endl;
             cin >> Weight;
             cout << "Чем она будет питаться: " << Foot << endl;
             cin >> Foot;
         }
 
-        void InfoFriendlyFish() {
+        void InfoFriendlyFish() const {
             cout << "Обычная рыба" << endl;
             cout << "Ваша рыбка: " << Frendlyfishs << endl;
             cout << "Размер рыбки: " << Size << endl;
             cout << "Вид рыбы: " << View << endl;
-            cout << "Вес рыбкы: " << Weight << endl;
+            cout << "Вес рыбы: " << Weight << endl;
             cout << "Чем она будет питаться: " << Foot << endl;
-           
         }
     };
 
@@ -70,8 +69,8 @@ public:
             cin >> Foot;
             cout << "Ваша рыба добавлена в аквариум" << endl;
         }
-        
-        void InfoFuckingFish() {
+
+        void InfoFuckingFish() const {
             cout << "Обычная рыба" << endl;
             cout << "Ваша рыбка: " << Fuckngfishs << endl;
             cout << "Размер рыбки: " << Size << endl;
@@ -84,7 +83,6 @@ public:
     class Vodorosli {
     private:
         string Kraken;
-
     public:
         Vodorosli(string kraken)
             :Kraken(kraken) {}
@@ -95,13 +93,26 @@ public:
         }
     };
 
+    void InfoAllFish(const vector<FriendlyFish>& friendlyFishes, const vector<FuckingFish>& fuckingFishes) {
+        cout << "Информация о всех рыбах:" << endl;
+
+        cout << "Добрые рыбки (" << friendlyFishes.size() << "):" << endl;
+        for (const auto& fish : friendlyFishes) {
+            fish.InfoFriendlyFish();
+        }
+
+        cout << endl << "Злые рыбки (" << fuckingFishes.size() << "):" << endl;
+        for (const auto& fish : fuckingFishes) {
+            fish.InfoFuckingFish();
+        }
+    }
 };
 
 int main() {
     setlocale(LC_ALL, "Rus");
 
-    Akvarium::FriendlyFish fish1("", 0, "", 0, "");
-    Akvarium::FuckingFish fish2("", 0, "", 0, "");
+    vector<Akvarium::FriendlyFish> friendlyFishes;
+    vector<Akvarium::FuckingFish> fuckingFishes;
     Akvarium::Vodorosli foot("");
 
     int choice;
@@ -109,32 +120,64 @@ int main() {
         setlocale(LC_ALL, "Rus");
 
         cout << "Добро пожаловать в аквариум Рамиля и Никиты!" << endl;
-        cout << "1.Добавить добрую рыбу: " << endl;
-        cout << "2.Информация о доброй рыбке: " << endl;
-        cout << "3.Добавить злую рыбку: " << endl;
-        cout << "4.Информация о хищной рыбке: " << endl;
-        cout << "5.Добавить водоросли: " << endl;
-        cout << "6.Выход из программы: " << endl;
+        cout << "1. Добавить добрую рыбу" << endl;
+        cout << "2. Информация о доброй рыбке" << endl;
+        cout << "3. Добавить злую рыбку" << endl;
+        cout << "4. Информация о хищной рыбке" << endl;
+        cout << "5. Добавить водоросли" << endl;
+        cout << "6. Вывести информацию о всех рыбах" << endl;
+        cout << "7. Выход из программы" << endl;
+
 
         cin >> choice;
 
+        if (!(cin >> choice)) {
+            cout << "\nОшибка: Неверный ввод. Пожалуйста, введите число.\n" << endl;
+            cin.clear();
+            cin.ignore(1000, '\n');
+            continue;
+        }
+
         switch (choice) {
-        case 1:
-            fish1.CreateFriendlyFish();
+        case 1: {
+            Akvarium::FriendlyFish newFriendlyFish("", 0, "", 0, "");
+            newFriendlyFish.CreateFriendlyFish();
+            friendlyFishes.push_back(newFriendlyFish);
             break;
+        }
         case 2:
-            fish1.InfoFriendlyFish();
+            if (!friendlyFishes.empty()) {
+                for (const auto& fish : friendlyFishes) {
+                    fish.InfoFriendlyFish();
+                }
+            }
+            else {
+                cout << "Нет добрых рыб" << endl;
+            }
             break;
-        case 3:
-            fish2.CreateFuckingFish();
+        case 3: {
+            Akvarium::FuckingFish newFuckingFish("", 0, "", 0, "");
+            newFuckingFish.CreateFuckingFish();
+            fuckingFishes.push_back(newFuckingFish);
             break;
+        }
         case 4:
-            fish2.InfoFuckingFish();
+            if (!fuckingFishes.empty()) {
+                for (const auto& fish : fuckingFishes) {
+                    fish.InfoFuckingFish();
+                }
+            }
+            else {
+                cout << "Нет злых рыб" << endl;
+            }
             break;
         case 5:
             foot.InfoVodorosli();
             break;
         case 6:
+            Akvarium().InfoAllFish(friendlyFishes, fuckingFishes);
+            break;
+        case 7:
             cout << "Вы успешно вышли из программы" << endl;
             break;
         default:
@@ -142,7 +185,7 @@ int main() {
             break;
         }
         cout << endl;
-    } while (choice != 6);
+    } while (choice != 7);
 
     return 0;
 }
